30a91d35a146478363c1ffd23973e5d350bd2d9c
mfonner <matt@matthewfonner.com>
2021-03-17 19:36:05 -0400

    Added reading from key from file.

--- woot.py
+++ woot.py
@@ -2,10 +2,15 @@ import requests
 import json
 
 
+def load_key(key_file):
+    with open(key_file) as f:
+        api_key = "".join(f.readlines())
+    return api_key
+
 url = "https://developer.woot.com"
 payload = {"x-api-key" : "API key goes here"}
 
-r = requests.get(url+"/feed/home", headers={"x-api-key":"API key goes here"})
+r = requests.get(url+"/feed/home", headers={"x-api-key": load_key("./api_key")})
 
 json_object = r.json()
 
