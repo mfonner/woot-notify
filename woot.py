6bfdfef0c0648dd36b278855c70fdd46bb4253df
mfonner <matt@matthewfonner.com>
2021-03-17 19:43:28 -0400

    Cleaned up API urls, added TODOs for future work

--- woot.py
+++ woot.py
@@ -2,16 +2,21 @@ import requests
 import json
 
 
+#TODO: Return results only matching search criteria i.e. mattress
+#TODO: Email those results to user
+#TODO: Args?
+
+
 def load_key(key_file):
     with open(key_file) as f:
         api_key = "".join(f.readlines())
     return api_key
 
+
 url = "https://developer.woot.com"
-payload = {"x-api-key" : "API key goes here"}
+feed_name = "/feed/home"
 
-r = requests.get(url+"/feed/home", headers={"x-api-key": load_key("./api_key")})
-
+r = requests.get(url+feed_name, headers={"x-api-key": load_key("./api_key")})
 json_object = r.json()
 
 for item in json_object['Items']:
